8674dfe9d7ffaa4c3e02f3448ee57322db7b81af
Казачек Иван <fishforbeer1@mail.ru>
2025-10-06 19:49:25 +0500

    Task0

---
+++ Tyuiu.KazachekI.Sprint3.Task0.V25.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KazachekI.Sprint3.Task0.V25.Lib
+{
+    public class DataService : ISprint3Task0V25
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sum = 0;
+
+            double constant = 4.0 / (1 + Math.Pow(value, 2));
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double term = Math.Pow(constant, i);
+                sum += term;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KazachekI.Sprint3.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KazachekI.Sprint3.Task0.V25.Lib;
+
+namespace Tyuiu.KazachekI.Sprint3.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+
+            double a1 = 4.0 / 26;
+            double q = a1;
+            double expected = a1 * (1 - Math.Pow(q, 14)) / (1 - q);
+
+            Assert.AreEqual(expected, res, 0.0001);
+        }
+
+
+        
+    }
+}
