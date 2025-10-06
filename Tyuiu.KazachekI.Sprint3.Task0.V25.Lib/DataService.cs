1b8c1958351fbd2fd6fac6185268451e7ed4ca27
Казачек Иван <fishforbeer1@mail.ru>
2025-10-06 19:55:30 +0500

    Task0

--- Tyuiu.KazachekI.Sprint3.Task0.V25.Lib/DataService.cs
+++ Tyuiu.KazachekI.Sprint3.Task0.V25.Lib/DataService.cs
@@ -9,15 +9,16 @@ namespace Tyuiu.KazachekI.Sprint3.Task0.V25.Lib
         {
             double sum = 0;
 
-            double constant = 4.0 / (1 + Math.Pow(value, 2));
-
+            // Используем цикл for для вычисления суммы ряда
             for (int i = startValue; i <= stopValue; i++)
             {
-                double term = Math.Pow(constant, i);
+                // Вычисляем i-й член ряда: (4/(1 + x^i))^i
+                double denominator = 1 + Math.Pow(value, i);
+                double term = Math.Pow(4.0 / denominator, i);
                 sum += term;
             }
 
-            return sum;
+            return Math.Round(sum, 2); 
         }
     }
 }
--- Tyuiu.KazachekI.Sprint3.Task0.V25.Test/DataServiceTest.cs
+++ Tyuiu.KazachekI.Sprint3.Task0.V25.Test/DataServiceTest.cs
@@ -15,15 +15,21 @@ namespace Tyuiu.KazachekI.Sprint3.Task0.V25.Test
             int stopValue = 14;
             double res = ds.GetSumSeries(value, startValue, stopValue);
 
-
-            double a1 = 4.0 / 26;
-            double q = a1;
-            double expected = a1 * (1 - Math.Pow(q, 14)) / (1 - q);
-
-            Assert.AreEqual(expected, res, 0.0001);
+            double wait = 0.69;
+            Assert.AreEqual(wait, res, 0.01);
         }
 
+        [TestMethod]
+        public void ValidGetSumSeriesFirstFewTerms()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 3;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
 
-        
+            double wait = 0.69;
+            Assert.AreEqual(wait, res, 0.01);
+        }
     }
 }
