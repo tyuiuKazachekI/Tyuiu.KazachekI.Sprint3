fc1bacadd51bf2a135a394cd66a9328b77a888a4
Казачек Иван <fishforbeer1@mail.ru>
2025-10-07 07:59:22 +0500

    TASK2

--- Tyuiu.KazachekI.Sprint3.Task2.V8.Test/DataServiceTest.cs
+++ Tyuiu.KazachekI.Sprint3.Task2.V8.Test/DataServiceTest.cs
@@ -1,11 +1,25 @@
-﻿namespace Tyuiu.KazachekI.Sprint3.Task2.V8.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KazachekI.Sprint3.Task2.V8.Lib;
+
+namespace Tyuiu.KazachekI.Sprint3.Task2.V8.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidGetMultiplySeries()
         {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            // Проверяем корректность вычислений
+            // Все множители положительные и меньше 1, произведение должно быть положительным и меньше 1
+            Assert.IsTrue(res > 0);
+            Assert.IsTrue(res < 1);
         }
+
+
     }
 }
